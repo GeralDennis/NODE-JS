2b609060edffa9f9e2ecb6f7ddc2eb8322b25adf
GeralDennis <50561312+GeralDennis@users.noreply.github.com>
2022-05-19 10:53:11 -0500

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -6,12 +6,12 @@ const memberSchema = new Schema({
 });
 const Member = mongoose.model("member", memberSchema);
 
-mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlParser: true });
+mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/NODOS", { useNewUrlParser: true });
 const http = require('http');
 http.createServer((req, res) => {
-    Member.find({}, "firstName lastName").then(members => {
-        if (members !== null && members.length > 0) {
-            res.write(JSON.stringify(members));
+    Member.find({}, "firstName lastName").then(INVERNADERO1 => {
+        if (INVERNADERO1 !== null && INVERNADERO1.length > 0) {
+            res.write(JSON.stringify(INVERNADERO1));
         } else {
             res.write("No members found");
         }
