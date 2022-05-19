5fc7659343e6d4ece7b965eca0d25a2e332776b9
GeralDennis <50561312+GeralDennis@users.noreply.github.com>
2022-05-18 21:07:32 -0500

    Create insert.js

---
+++ insert.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+var Schema1 = { firstName: "Bill", lastName: "Smith" };
+const Schema = db.members.insert(Schema1);
+const memberSchema = new Schema({
+    firstName: String,
+    lastName: String
+});
+const Member = mongoose.model("member", memberSchema);
+
+mongoose.connect("mongodb://my_user:my_pwd@localhost:27017/mern", { useNewUrlParser: true });
+const http = require('http');
+http.createServer((req, res) => {
+    Member.find({}, "firstName lastName").then(members => {
+        if (members !== null && members.length > 0) {
+            res.write(JSON.stringify(members));
+        } else {
+            res.write("No members found");
+        }
+        res.end();
+    });
+}).listen(8000);
